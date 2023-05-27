065b8dd6a5138e47cc430d56b52cbca0c9129881
x1a0t <405028157@qq.com>
2023-05-27 19:23:35 +0800

    init

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.jsx',
+  staticImage: true,
+  defaultShowCopyCode: true,
+  readingTime: true
+})
+
+export default withNextra({
+  reactStrictMode: true,
+  cleanDistDir: true
+})
