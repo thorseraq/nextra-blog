3bd5c0bc354ac1ff627e3689bd74bb978115fb2b
x1a0t <thorseraq@gmail.com>
2023-06-10 00:17:44 +0800

    chore: update

---
+++ pages/_posts/index.md
@@ -0,0 +1,7 @@
+---
+type: _posts
+title: Random Thoughts
+date: 2020-01-03
+---
+
+# Random Thoughts
---
+++ theme.config.jsx
@@ -0,0 +1,66 @@
+/* eslint sort-keys: error */
+export default {
+    components: {
+        h1: ({children}) => (
+            <h1
+                style={{
+                    WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundClip: 'text',
+                    backgroundImage: 'linear-gradient(90deg,#7928CA,#FF0080)'
+                }}
+            >
+                {children}
+            </h1>
+        ),
+        blockquote: ({children}) => (
+            <blockquote
+                style={{
+                    WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundClip: 'text',
+                    backgroundImage: 'linear-gradient(120deg, #008080, #008050)'
+                }}
+            >
+                {children}
+            </blockquote>
+        ),
+        strong: ({children}) => (
+            <strong
+                style={{
+                    WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'rgba(119,72,176,0.7)',
+                    backgroundClip: 'text',
+                    // backgroundImage: 'linear-gradient(120deg, #008080, #8080cc)'
+                }}
+            >
+                {children}
+            </strong>
+        ),
+    },
+    darkMode: true,
+    dateFormatter: date => `Last updated at ${date.toDateString()}`,
+    footer: (
+        <small style={{display: 'block', marginTop: '8rem'}}>
+            <abbr
+                title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
+                style={{cursor: 'help'}}
+            >
+                CC BY-NC 4.0
+            </abbr>{' '}
+            <a href="https://github.com/shuding/nextra">{new Date().getFullYear()} © Powered by Nextra. </a>
+            <style jsx>{`
+              a {
+                float: right;
+              }
+
+              @media screen and (max-width: 480px) {
+                article {
+                  padding-top: 2rem;
+                  padding-bottom: 4rem;
+                }
+              }
+            `}</style>
+        </small>
+    )
+}
